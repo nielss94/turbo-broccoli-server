f61e99f35b61e938b0ee0472c2f94127f905751b
nielss94 <nielsvdam@hotmail.com>
2017-12-12 19:56:56 +0100

    neo4j online

--- config/neo4j.db.js
+++ config/neo4j.db.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j');
 
-const db = new neo4j.GraphDatabase('http://Niels:root@localhost:7474');
+const db = new neo4j.GraphDatabase('http://Niels:b.V59qvPCc1Lqq.pWgBksSC2TUAkkxt@hobby-dmgobjifjhecgbkeiiligjal.dbs.graphenedb.com:24789');
 
 module.exports = db;
